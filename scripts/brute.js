f94e162cdb644ce32ab12c15e145af2517b119e1
shreya-kulkarni <shreyagk2000@gmail.com>
2020-08-19 12:44:24 +0530

    update

--- scripts/brute.js
+++ scripts/brute.js
@@ -1,4 +1,5 @@
- var grid = 30;
+//variable declaration
+var grid = 30;
 var gridOffset = grid / 2;
 var locations = [];
 var totalLocations;
@@ -7,147 +8,114 @@ var recordDistance;
 var bestEver=[];
 var iteration=0;
 var click=0;
+//get the no.of locations
 function bstart(temp)
 {
-totalLocations=temp;
-console.log(totalLocations);
+  totalLocations=temp;
+  console.log(totalLocations);
 }
 
-function setup() {
- 
-
+//setup canvas
+function setup() 
+{
   createCanvas(1455, 550);
   background(0);
-   frameRate(3);
-  // Draw grid
+  frameRate(3);
+  drawgrid();
+}
+
+
+function drawgrid()
+{
   var l = 0;
   strokeWeight(1);
   stroke(151);
-  while (l < width || l < height) {
+  while (l < width || l < height) 
+  {
     line(0, l, width, l);
     line(l, 0, l, height);
     l += grid;
   }
   strokeWeight(25);
   line(width/2,0,width/2,height)
-
 }
 
 
 function mousePressed()
 {
   if(count<totalLocations && mouseX)
-   {strokeWeight(10); 
+   { 
+    strokeWeight(10); 
     stroke('white');
     point(mouseX, mouseY, 10, 10);
     var v=createVector(mouseX,mouseY);
     locations.push(v);
     count++;
     text(`Mark more : ${(totalLocations-count)} locations`,10,50)
-}
-if(count==totalLocations)
-{
-  var d = calcDistance(locations);
+  }
+  if(count==totalLocations)
+  {
+    var d = calcDistance(locations);
     recordDistance = d;
     bestEver = locations.slice();
-
+  }
 }
-}
 
-
-
-
- 
-
-function draw() {
-  //background(bg);
-  var l = 0;
-  iteration++;
-  strokeWeight(1);
-  stroke(151);
-  while (l < width || l < height) {
-    line(0, l, width, l);
-    line(l, 0, l, height);
-    l += grid;
-  } 
-  strokeWeight(25);
-  line(width/2,0,width/2,height)
-
-    if(count==totalLocations)
+function draw() 
+{
+  drawgrid();
+  if(count==totalLocations)
   {
-    
-
-  background(0);
-  fill(0);
-  console.log(recordDistance);
-  strokeWeight(10);
-  text(`This is our best path
+    background(0);
+    fill(0);
+    console.log(recordDistance);
+    strokeWeight(10);
+    text(`This is our best path
     Best Distance: ${(recordDistance)} kms!`,width/4,50)
-  var l = 0;
-  strokeWeight(1);
-  stroke(151);
-  while (l < width || l < height) {
-    line(0, l, width, l);
-    line(l, 0, l, height);
-    l += grid;
-  }
-
-   strokeWeight(25);
-  line(width/2,0,width/2,height)
-
-
-
-  fill(255);
-  for (var i = 0; i < locations.length; i++) {
-    strokeWeight(20); 
-    point(locations[i].x, locations[i].y, 8, 8);
-    
-  }
+    drawgrid();
+    fill(255);
+    for (var i = 0; i < locations.length; i++) {
+      strokeWeight(20); 
+      point(locations[i].x, locations[i].y, 8, 8);
+    }
   
   stroke('white');
   strokeWeight(2);
   noFill();
   beginShape();
-  for (var i = 0; i < locations.length; i++) {
+  for (var i = 0; i < locations.length; i++)
+  {
     vertex(bestEver[i].x, bestEver[i].y);
     stroke('white');
     text(i+1,bestEver[i].x+10,bestEver[i].y+10);
   }
   endShape();
-
- translate(width/2, 0);
-
- stroke('red');
+  translate(width/2, 0);
+  stroke('red');
   strokeWeight(2);
   noFill();
   beginShape();
-  for (var i = 0; i < locations.length; i++) {
+  for (var i = 0; i < locations.length; i++) 
+  {
     ellipse(locations[i].x, locations[i].y, 8, 8);
     vertex(locations[i].x, locations[i].y);
     text(i+1,bestEver[i].x+10,bestEver[i].y+10);
   }
   endShape();
-  
-  
 
   var i = floor(random(locations.length));
   var j = floor(random(locations.length));
   swap(locations, i, j);
 
   var d = calcDistance(locations);
-  if (d < recordDistance) {
+  if (d < recordDistance) 
+  {
     recordDistance = d;
     bestEver = locations.slice();
     
   }
-  }
-  
-
-
-
 }
-
-
+}
 
 function swap(a, i, j) {
   var temp = a[i];
@@ -165,8 +133,6 @@ function calcDistance(points) {
 }
 function check()
 {
-  
   noLoop();
-  
 }
 
--- scripts/genetic.js
+++ scripts/genetic.js
@@ -20,18 +20,19 @@ var click=0;
 function start(temp)
 {
 totalLocations=temp;
-
 console.log(totalLocations);
 }
 
 //setup background gird
-function setup() {
-
+function setup()
+{
   createCanvas(1455, 550);
   background(0);
-   frameRate(5);
-  
-  // Draw grid
+  frameRate(5);
+}
+function drawgrid()
+{
+ // Draw grid
   var l = 0;
   strokeWeight(1);
   stroke(151);
@@ -51,118 +52,90 @@ function setup() {
 function mousePressed()
 {
   if(count<totalLocations && mouseX)
-   {strokeWeight(10); 
+   {
+    strokeWeight(10); 
     stroke('white');
     point(mouseX, mouseY, 10, 10);
     var v=createVector(mouseX,mouseY);
     locations.push(v);
     count++;
      text(`Mark ${(totalLocations-count)} more locations`,10,50)
-    {for(var i=0;i<totalLocations;i++)
-  {
-    order[i]=i;
+     {for(var i=0;i<totalLocations;i++)
+      {
+        order[i]=i;
+      }
+      for (var i = 0; i < popSize; i++) {
+        population[i] = shuffle(order);
+      }
+    }
   }
-  for (var i = 0; i < popSize; i++) {
-    population[i] = shuffle(order);
-  }}
-}
 }
 
 
 function draw() {
-
-  var l = 0;
-  iteration++;
-  strokeWeight(1);
-  stroke(151);
-  while (l < width || l < height) {
-    line(0, l, width, l);
-    line(l, 0, l, height);
-    l += grid;
-  }strokeWeight(25);
-  line(width/2,0,width/2,height)
+  drawgrid();
   strokeWeight(10);
   text(`This is our best path
     Best Distance: ${(recordDistance)} kms!`,width/4,50)
   strokeWeight(10);
   text(`Current Total Distance of the given path: ${(current)} kms!`,3*width/4-30,50)
 
-
-    if(count==totalLocations)
+  if(count==totalLocations)
   {
-    //if(iteration==1)
-    
-  
-  background(0);
-  fill(0);
-  noStroke();
-  textSize(12);
-  
-  console.log(recordDistance);
-  var l = 0;
-  strokeWeight(1);
-  stroke(151);
-  while (l < width || l < height) {
-    line(0, l, width, l);
-    line(l, 0, l, height);
-    l += grid;
-  }
-
-
-  strokeWeight(25);
-  line(width/2,0,width/2,height)
-  strokeWeight(10);
-  text(`This is our best path
+    background(0);
+    fill(0);
+    noStroke();
+    textSize(12);
+    console.log(recordDistance);
+    drawgrid();
+    strokeWeight(10);
+    text(`This is our best path
     Best Total Distance: ${(recordDistance)} kms!`,width/4,50)
-  strokeWeight(10);
-  text(`Current Total Distance of the given path: ${(current)} kms!`,3*width/4-30,50)
+    strokeWeight(10);
+    text(`Current Total Distance of the given path: ${(current)} kms!`,3*width/4-30,50)
 
 
   //use genetic algorithm functions
-  calculateFitness();
-  normalizeFitness();
-  nextGeneration();
+    calculateFitness();
+    normalizeFitness();
+    nextGeneration();
 
 
   //mark the locations coordinate
-  fill(255);
-  for (var i = 0; i < locations.length; i++) {
-    strokeWeight(10); 
-    point(locations[i].x, locations[i].y, 5, 5);
-    
-  }
+    fill(255);
+    for (var i = 0; i < locations.length; i++) {
+      strokeWeight(10); 
+      point(locations[i].x, locations[i].y, 5, 5);
+    }
  
   //display the best path
-  stroke('white');
-  strokeWeight(2);
-  noFill();
-  beginShape();
+    stroke('white');
+    strokeWeight(2);
+    noFill();
+    beginShape();
 
-  for (var i = 0; i < bestEver.length; i++) {
-    var n = bestEver[i];
-    vertex(locations[n].x, locations[n].y);
-    point(locations[n].x, locations[n].y, 5, 5);
-    text(i+1,locations[n].x+10,locations[n].y+10);
+    for (var i = 0; i < bestEver.length; i++) {
+      var n = bestEver[i];
+      vertex(locations[n].x, locations[n].y);
+      point(locations[n].x, locations[n].y, 5, 5);
+      text(i+1,locations[n].x+10,locations[n].y+10);
+    }
+    endShape();
+
+    translate(width/2, 0);
+
+    stroke('red');
+    strokeWeight(1);
+    noFill();
+    beginShape();
+    for (var i = 0; i < currentBest.length; i++) {
+      var n = currentBest[i];
+      vertex(locations[n].x, locations[n].y);
+      ellipse(locations[n].x, locations[n].y, 5, 5);
+      text(i+1,locations[n].x+10,locations[n].y+10);
+    }
+    endShape();
   }
-  endShape();
-
-  translate(width/2, 0);
-
-  //display the current path
-  stroke('red');
-  strokeWeight(1);
-  noFill();
-  beginShape();
-  for (var i = 0; i < currentBest.length; i++) {
-    var n = currentBest[i];
-    vertex(locations[n].x, locations[n].y);
-    ellipse(locations[n].x, locations[n].y, 5, 5);
-     text(i+1,locations[n].x+10,locations[n].y+10);
-  }
-  endShape();
-}
-  
- 
 }
 
 function swap(a, i, j) {
